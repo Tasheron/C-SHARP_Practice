85b9a888dc8c9cb7f545d788a14fdc23bff4ec8a
Tasheron <alex-2004.ru@yandex.ru>
2021-12-26 14:35:54 +0300

    program is working

--- Program.cs
+++ Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] resizeArray(int[] array){
+    int[] arrayNew = new int[array.Length];
+    int i = 0;
+    foreach(int element in array){
+        if(element % 2 == 0){
+            arrayNew[i] = element;
+            i++;
+        }
+    }
+    for (int j = i; j < arrayNew.Length; j++)
+        arrayNew[j] = -10;
+    return arrayNew;
+}
+
+void printArray(int[] array){
+    foreach(int el in array)
+        if(el > -10)
+            Console.Write(el + " ");
+    Console.WriteLine();
+}
+
+int[] array = new int[10];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-9, 10);
+}
+
+Console.WriteLine("Изначальный массив: ");
+printArray(array);
+
+int[] arrayNew = resizeArray(array);
+
+Console.WriteLine("Массив четных чисел: ");
+printArray(arrayNew);
